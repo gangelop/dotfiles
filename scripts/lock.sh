fe4562bcc20e7c13f3495535a9768e43985c38e8
George Angelopoulos <george@usermod.net>
2015-08-19 09:15:01 +0200

    i3: new locking script with pixelation effect
    Thnx @savoca ! :D

---
+++ scripts/lock.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# all credit and glory for this awesome script to
+# github.com/savoca
+# reddit.com/u/wontonspecial
+#
+# https://github.com/savoca/dotfiles/blob/dark/.bin/scripts/lock.sh
+# https://www.reddit.com/r/unixporn/comments/3358vu/i3lock_unixpornworthy_lock_screen/
+
+tmpbg="/tmp/screen.png"
+#icon="/home/george/lock_icon.png"
+scrot $tmpbg
+convert $tmpbg -scale 10% -scale 1000% -fill black -colorize 25% $tmpbg
+#[[ -f $icon ]] && convert $tmpbg $icon -gravity center -composite -matte $tmpbg
+#dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Stop
+killall -SIGUSR1 dunst; i3lock -u --nofork -i $tmpbg && killall -SIGUSR2 dunst
