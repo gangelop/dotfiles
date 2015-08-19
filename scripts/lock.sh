6951997768dd03d46d1e597e6aa1c6c7261c044a
George Angelopoulos <george@usermod.net>
2015-08-19 12:05:08 +0200

    lock.sh: more pixelation

--- scripts/lock.sh
+++ scripts/lock.sh
@@ -10,7 +10,7 @@
 tmpbg="/tmp/screen.png"
 #icon="/home/george/lock_icon.png"
 scrot $tmpbg
-convert $tmpbg -scale 10% -scale 1000% -fill black -colorize 25% $tmpbg
+convert $tmpbg -scale 5% -scale 2005% -fill black -colorize 25% $tmpbg
 #[[ -f $icon ]] && convert $tmpbg $icon -gravity center -composite -matte $tmpbg
 #dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Stop
 killall -SIGUSR1 dunst; i3lock -u --nofork -i $tmpbg && killall -SIGUSR2 dunst
