3852814773039a40f860a3c6ff0b305ca6f93f09
George Angelopoulos <george@usermod.net>
2015-08-19 17:47:58 +0200

    lock.sh: properly handle the tmp file with mktemp

--- scripts/lock.sh
+++ scripts/lock.sh
@@ -7,10 +7,11 @@
 # https://github.com/savoca/dotfiles/blob/dark/.bin/scripts/lock.sh
 # https://www.reddit.com/r/unixporn/comments/3358vu/i3lock_unixpornworthy_lock_screen/
 
-tmpbg="/tmp/screen.png"
+tmpbg="$(mktemp /tmp/lock.sh-XXXXXXXXXX.png)"
 #icon="/home/george/lock_icon.png"
 scrot $tmpbg
 convert $tmpbg -scale 5% -scale 2005% -fill black -colorize 25% $tmpbg
 #[[ -f $icon ]] && convert $tmpbg $icon -gravity center -composite -matte $tmpbg
 #dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Stop
 killall -SIGUSR1 dunst; i3lock -u --nofork -i $tmpbg && killall -SIGUSR2 dunst
+rm $tmpbg
