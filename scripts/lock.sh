c0348f34f81bb30656818f7dfc3c23afa777df0b
George Angelopoulos <george@usermod.net>
2015-10-16 13:47:47 +0200

    lock.sh: re-enable the i3lock indicator
    It's actually useful. I prefer to have it enabled.

--- scripts/lock.sh
+++ scripts/lock.sh
@@ -21,6 +21,6 @@ convert $tmpbg -scale 10% -scale 1000% -fill black -colorize 25% $tmpbg
 # to prevent flickering of notifications through the lock and
 # to read all missed notifications after returning to the computer.
 killall -SIGUSR1 dunst
-i3lock -u --nofork -i $tmpbg
+i3lock --nofork -i $tmpbg
 killall -SIGUSR2 dunst
 rm $tmpbg
