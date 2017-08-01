915a6576da3c9ab45c9bdb1ba6b78b7a1ce71b03
George Angelopoulos <george@usermod.net>
2017-08-01 19:33:14 +0200

    add magnet.sh script for firefox magnet links

---
+++ scripts/magnet.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+set -o nounset
+
+transmission-remote --add "$1" && notify-send -i transmission 'torrent added' || notify-send -i transmission -u critical 'Failed to add torrent!' 'Is transmission running?'
