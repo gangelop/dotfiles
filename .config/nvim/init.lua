bda9305043ca12387b657b7c5548de6c054469e6
George Angelopoulos <george@usermod.net>
2023-06-15 10:39:27 +0300

    nvim: add lua lsp

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -140,5 +140,28 @@ vim.api.nvim_create_autocmd({ "InsertLeave", "BufWritePre" }, {
     command = "lua vim.lsp.buf.code_action({ context = { only = { 'source.organizeImports' } }, apply = true })"
 })
 
+lspconfig.lua_ls.setup {
+  settings = {
+    Lua = {
+      runtime = {
+        -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+        version = 'LuaJIT',
+      },
+      diagnostics = {
+        -- Get the language server to recognize the `vim` global
+        globals = {'vim'},
+      },
+      workspace = {
+        -- Make the server aware of Neovim runtime files
+        library = vim.api.nvim_get_runtime_file("", true),
+      },
+      -- Do not send telemetry data containing a randomized but unique identifier
+      telemetry = {
+        enable = false,
+      },
+    },
+  },
+}
+
 -- }}}
 -- vim:fdm=marker
