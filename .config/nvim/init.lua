d46438fd0cfbbc552af561bb812ba6148e44fed6
George Angelopoulos <george@usermod.net>
2025-10-28 14:39:11 +0200

    some update broke this

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -530,16 +530,16 @@ mason_lspconfig.setup {
   ensure_installed = vim.tbl_keys(servers),
 }
 
-mason_lspconfig.setup_handlers {
-  function(server_name)
-    require('lspconfig')[server_name].setup {
-      capabilities = capabilities,
-      on_attach = on_attach,
-      settings = servers[server_name],
-      filetypes = (servers[server_name] or {}).filetypes,
-    }
-  end
-}
+-- mason_lspconfig.setup_handlers {
+--   function(server_name)
+--     require('lspconfig')[server_name].setup {
+--       capabilities = capabilities,
+--       on_attach = on_attach,
+--       settings = servers[server_name],
+--       filetypes = (servers[server_name] or {}).filetypes,
+--     }
+--   end
+-- }
 
 vim.api.nvim_create_autocmd("BufWritePre", {
     pattern = "*.go",
